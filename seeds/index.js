bb6ce936f023e8a9e280b73ba41919ce9640f002
Rianail <coulson.ryan@gmail.com>
2021-06-16 10:35:59 +0200

    seeds for real

--- seeds/index.js
+++ seeds/index.js
@@ -11,7 +11,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(dbUrl, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
